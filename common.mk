6eed4e393e217f6e87a81873cd31bd7a4aaa3e75
Kunmun <kunmun@aospa.co>
2023-03-09 19:20:23 +0530

    Makefile: Build MotoCommonOverlay
    Change-Id: I683bceb5658bbcca488ebfef6d07c78bbc2510a1

---
+++ common.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+HARDWARE_PATH := hardware/motorola
+
+# Overlay
+PRODUCT_PACKAGES += \
+    MotoCommonOverlay
