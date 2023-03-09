aac809f4b041e7951aafcf8f384b2c814a735d6e
Kunmun <kunmun@aospa.co>
2023-03-09 19:30:30 +0530

    Makefile: Conditionally build biometrics HAL and it's overlays
    Change-Id: I17610eba338b58fd15c3f2b3d7674dde4962a9b5

--- common.mk
+++ common.mk
@@ -9,3 +9,13 @@ HARDWARE_PATH := hardware/motorola
 # Overlay
 PRODUCT_PACKAGES += \
     MotoCommonOverlay
+
+ifeq ($(TARGET_BUILDS_OSS_BIOMETRICS),true)
+# Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.motorola
+
+# UDFPS Overlays
+PRODUCT_PACKAGES += \
+    MotoUDFPSOverlay
+endif
